c56ef5d0a94afc659ce296b284389f36beafd5b8
WildBabaloo <andphiltre@gmail.com>
2024-11-26 22:19:00 -0500

    Add code card onto codes page

--- app/codes/page.tsx
+++ app/codes/page.tsx
@@ -1,11 +1,23 @@
 "use client";
 import NavBarUI from "@/components/ui/NavBar";
+import Card from "@/components/CodeCard";
+import Ecopoint from "@/images/Map_Images/EcoPointAntarctica.png";
 
 export default function Codes() {
   return (
     <>
       <NavBarUI />
-      Codes Page
+      <div className="flex items-center justify-center min-h-screen bg-gray-800 p-4">
+        <Card
+          title="Mercy Parkour Ep: Antarctica"
+          code="QTXG4"
+          difficulty="Intermediate"
+          mapper="OnMyKnees"
+          date="12 Oct 2024"
+          likes={36}
+          imageSrc={Ecopoint}
+        />
+      </div>
     </>
   );
 }
