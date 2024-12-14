5abe50b8ba5d2804f900782333056f0894a82b21
WildBabaloo <andphiltre@gmail.com>
2024-12-14 16:54:32 -0500

    Route handler now supports seached results

--- app/api/codes/route.ts
+++ app/api/codes/route.ts
@@ -1,20 +1,32 @@
-import prisma from "@/prisma/lib/db";
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/lib/db";
 
 export async function GET(request: NextRequest) {
     try {
         const url = new URL(request.url);
+        const search = url.searchParams.get("search");
         const skip = url.searchParams.get("skip");
         const take = url.searchParams.get("take");
-        console.log(`Skip: ${skip}, Take: ${take}`);
-        const skipInt = parseInt(skip as string, 10);
-        const takeInt = parseInt(take as string, 10);
+
+        console.log(`Skip: ${skip}, Take: ${take}, Search: ${search}`);
+
+        const skipInt = parseInt(skip as string, 10) || 0; // Default to 0 if not provided
+        const takeInt = parseInt(take as string, 10) || 20; // Default to 20 if not provided
 
         if (isNaN(skipInt) || isNaN(takeInt)) {
-            return NextResponse.json({ error: "Invalid skip or take parameter" }, {status: 400});
+            return NextResponse.json({ error: "Invalid skip or take parameter" }, { status: 400 });
         }
 
         const codes = await prisma.mercy_parkour_codes.findMany({
+            where: search
+                ? {
+                      OR: [
+                          { Map: { contains: search, mode: "insensitive" } },
+                          { Code: { contains: search, mode: "insensitive" } },
+                          { Author: { contains: search, mode: "insensitive" } },
+                      ],
+                  }
+                : undefined,
             skip: skipInt,
             take: takeInt,
             orderBy: { Map_Number: "desc" },
@@ -23,7 +35,6 @@ export async function GET(request: NextRequest) {
         return NextResponse.json(codes);
     } catch (error) {
         console.error("Error fetching codes from the database", error);
-        NextResponse.json({ error: "Failed to fetch code data" }, {status: 400})
+        return NextResponse.json({ error: "Failed to fetch code data" }, { status: 500 });
     }
-
 }
