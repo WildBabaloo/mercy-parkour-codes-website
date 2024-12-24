9b74471aabfc4ac69bfe221835a7618c405c101c
WildBabaloo <andphiltre@gmail.com>
2024-12-23 22:55:26 -0500

    api route refactorisation

--- app/api/codes/route.ts
+++ app/api/codes/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-// import { getNewCodesDefault } from "@/sql/queries/codes/getNewCodesDefault";
-// import { getSortedMapCodes } from "@/sql/queries/codes/getSortedMapCodes";
-import prisma from "@/prisma/lib/db";
+// import prisma from "@/prisma/lib/db";
+import { getNewCodesDefault } from "@/sql/queries/codes/getNewCodesDefault";
+import { getSortedMapCodes } from "@/sql/queries/codes/getSortedMapCodes";
 
 export async function GET(request: NextRequest) {
     try {
@@ -19,67 +19,6 @@ export async function GET(request: NextRequest) {
         if (isNaN(skipInt) || isNaN(takeInt)) {
             return NextResponse.json({ error: "Invalid skip or take parameter" }, { status: 400 });
         }
-
-        /*
-
-        let codes;
-        if (!sortMethod) {
-          codes = await getNewCodesDefault(search, skipInt, takeInt);
-        } else {
-          const [sortKey, sortOrder] = sortMethod.split("_");
-          if (isValidSort(sortKey, sortOrder)) {
-            sortKey = "Map_Number"
-            sortOrder = "desc"
-          }
-          
-          codes = await getSortedMapCodes(search, skipInt, takeInt, sortKey, sortOrder)
-        }
-        */
-
-
-        const fetchCodes = async ({
-            search,
-            sort,
-            skip,
-            take,
-          }: {
-            search?: string;
-            sort?: string;
-            skip: number;
-            take: number;
-          }) => {
-            const [sortKey, sortOrder] = sort && sort !== "undefined"
-              ? sort.split("_")
-              : ["Map_Number", "desc"];
-
-              console.log(`Sort: ${sort}, SortKey: ${sortKey}, SortOrder: ${sortOrder}`);
-        
-              const codes = await prisma.mercy_parkour_codes.findMany({
-                where: {
-                  AND: [
-                    search
-                      ? {
-                          OR: [
-                            { Map: { contains: search, mode: "insensitive" } },
-                            { Code: { contains: search, mode: "insensitive" } },
-                            { Author: { contains: search, mode: "insensitive" } },
-                          ],
-                        }
-                      : {},
-                    {
-                      [sortKey]: {
-                        not: null,
-                      },
-                    },
-                  ],
-                },
-                orderBy: { [sortKey]: sortOrder },
-                skip,
-                take,
-              });
-        
-            return codes;
-          };
         
           const queryParams = {
             search: search || "",
@@ -97,21 +36,27 @@ export async function GET(request: NextRequest) {
     }
 }
 
-/* 
-enum SortKey {
-  Map = "Map",
-  Favorites = "Favorites",
-  Difficulty = "Difficulty",
-  Author = "Author",
-  Checkpoints = "Checkpoints",
-}
+const fetchCodes = async ({
+  search,
+  sort,
+  skip,
+  take,
+}: {
+  search?: string;
+  sort?: string;
+  skip: number;
+  take: number;
+}) => {
+  const [sortKey, sortOrder] = sort && sort !== "undefined"
+    ? sort.split("_")
+    : ["Map_Number", "desc"];
 
-enum SortOrder {
-  Asc = "asc",
-  Desc = "desc",
-}
+    console.log(`Sort: ${sort}, SortKey: ${sortKey}, SortOrder: ${sortOrder}`);
 
-const isValidSort = (key: string, direction: string): boolean => {
-  return key in SortKey && direction in SortOrder;
+      const codes =
+        sortKey === "Map_Number"
+          ? getNewCodesDefault(search, skip, take)
+          : getSortedMapCodes(search, skip, take, sortKey, sortOrder);
+
+  return codes;
 };
-*/
