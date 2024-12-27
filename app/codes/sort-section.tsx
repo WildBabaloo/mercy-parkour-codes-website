1b3e57c5b978dbc03e70778ba3f7e1dfa029eb17
WildBabaloo <andphiltre@gmail.com>
2024-12-27 16:11:33 -0500

    Added component onto pages

--- app/codes/sort-section.tsx
+++ app/codes/sort-section.tsx
@@ -69,22 +69,20 @@ export default function SortSection() {
   };
 
   return (
-    <div className="items-center justify-center flex">
-      <div className="text-white p-2 bg-zinc-700 max-w-screen-md w-full flex flex-wrap items-center gap-4 text-sm md:text-base rounded-md">
-        <span className="w-full md:w-auto ml-2">Sort by:</span>
-        {sortOptions.map((option) => (
-          <button
-            key={option}
-            onClick={() => {
-              handleSortClick(option);
-            }}
-            className="p-2 rounded-md hover:bg-zinc-600 transition duration-200 flex items-center gap-1"
-          >
-            {option}
-            <span className="ml-1">{getArrowIcon(option)}</span>
-          </button>
-        ))}
-      </div>
+    <div className="text-white p-2 bg-zinc-700 max-w-screen-md w-full flex flex-wrap items-center gap-4 text-sm md:text-base rounded-md">
+      <span className="w-full md:w-auto ml-2">Sort by:</span>
+      {sortOptions.map((option) => (
+        <button
+          key={option}
+          onClick={() => {
+            handleSortClick(option);
+          }}
+          className="p-2 rounded-md hover:bg-zinc-600 transition duration-200 flex items-center gap-1"
+        >
+          {option}
+          <span className="ml-1">{getArrowIcon(option)}</span>
+        </button>
+      ))}
     </div>
   );
 }
--- components/ui/CodeInput.tsx
+++ components/ui/CodeInput.tsx
@@ -8,54 +8,56 @@ import {
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { useDebounce } from "use-debounce";
 
-const CodeInput = forwardRef<HTMLInputElement, ComponentProps<"input">>(
-  ({ className, type, ...props }, ref) => {
-    const [text, setText] = useState("");
-    const searchParams = useSearchParams();
-    const pathname = usePathname();
-    const { replace } = useRouter();
-    const [searchTerm] = useDebounce(text, 500);
+const CodeInput = forwardRef<
+  HTMLInputElement,
+  ComponentProps<"input"> & { onMenuToggle?: () => void }
+>(({ className, type, onMenuToggle, ...props }, ref) => {
+  const [text, setText] = useState("");
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const [searchTerm] = useDebounce(text, 500);
 
-    useEffect(() => {
-      const params = new URLSearchParams(searchParams);
-      if (searchTerm) {
-        params.set("search", searchTerm);
-      } else {
-        params.delete("search");
-      }
-      replace(`${pathname}?${params.toString()}`);
-    }, [searchTerm, pathname, searchParams, replace]);
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams);
+    if (searchTerm) {
+      params.set("search", searchTerm);
+    } else {
+      params.delete("search");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, [searchTerm, pathname, searchParams, replace]);
 
-    return (
-      <div className="relative w-full">
-        {/* Input Bar */}
-        <input
-          type={type}
-          className={cn(
-            "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-            className
-          )}
-          ref={ref}
-          {...props}
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          defaultValue={searchParams.get("query")?.toString()}
+  return (
+    <div className="relative w-full">
+      {/* Input Bar */}
+      <input
+        type={type}
+        className={cn(
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          className
+        )}
+        ref={ref}
+        {...props}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        defaultValue={searchParams.get("query")?.toString()}
+      />
+      {/* Search Icon */}
+      <div className="absolute right-3 top-1/2 transform -translate-y-1/2 text-muted-foreground flex gap-4">
+        <AdjustmentsHorizontalIcon
+          className="h-5 w-5 text-gray-400"
+          aria-hidden="true"
+          onClick={onMenuToggle}
         />
-        {/* Search Icon */}
-        <div className="absolute right-3 top-1/2 transform -translate-y-1/2 text-muted-foreground flex gap-4">
-          <AdjustmentsHorizontalIcon
-            className="h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-          <MagnifyingGlassIcon
-            className="h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-        </div>
+        <MagnifyingGlassIcon
+          className="h-5 w-5 text-gray-400"
+          aria-hidden="true"
+        />
       </div>
-    );
-  }
-);
+    </div>
+  );
+});
 CodeInput.displayName = "CodeInput";
 
 export { CodeInput };
