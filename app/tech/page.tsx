20108a34b57c541a1a033eaa8802fd8b7df22489
WildBabaloo <andphiltre@gmail.com>
2025-01-02 23:14:54 -0500

    Style tech page

--- app/tech/page.tsx
+++ app/tech/page.tsx
@@ -1,18 +1,21 @@
 import NavBarUI from "@/components/ui/NavBar";
 import Image from "next/image";
+import Ecopoint from "@/images/Map_Images/EcoPointAntarctica.png";
+import Link from "next/link";
 
 export default function Tech() {
-  const techItems = [
-    { id: 1, title: "Slingshot", gif: "/slingshot.gif" },
-    { id: 2, title: "Superjump", gif: "/superjump.gif" },
-    { id: 3, title: "Bunnyhop", gif: "/bunnyhop.gif" },
+  const basicTechItems = [
+    { id: 1, title: "Slingshot", gif: Ecopoint, link: "/tech/slingshot" },
+    { id: 2, title: "Superjump", gif: Ecopoint, link: "/tech/superjump" },
+    { id: 3, title: "Bunnyhop (Bhop)", gif: Ecopoint, link: "/tech/bunnyhop" },
+    { id: 4, title: "GA Cancel Bhop", gif: Ecopoint, link: "/tech/bunnyhop" },
   ];
 
   return (
     <>
       <NavBarUI />
       <div className="bg-gray-800 p-4 text-white">
-        <div className="text-center mt-4 text-3xl text-white font-bold">
+        <div className="text-center m-6 text-3xl text-white font-bold">
           <h1>Hall Of Mercy Parkour Tech</h1>
         </div>
 
@@ -23,27 +26,29 @@ export default function Tech() {
         {/* Tech Items */}
         <div className="min-h-screen flex justify-center mt-2">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-screen-md h-full w-full align-top">
-            {techItems.map((item) => (
+            {basicTechItems.map((item) => (
               <div
                 key={item.id}
                 className="border rounded-md shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300"
               >
-                {/* GIF/Tech Image */}
-                <div className="h-40 bg-gray-200 flex items-center justify-center">
-                  <Image
-                    fill={true}
-                    src={item.gif}
-                    alt={item.title}
-                    className="object-cover h-full w-full"
-                  />
-                </div>
+                <Link href={item.link}>
+                  {/* GIF/Tech Image */}
+                  <div className="relative w-full h-48 overflow-hidden">
+                    <Image
+                      layout="fill"
+                      src={item.gif}
+                      alt={item.title}
+                      className="object-cover"
+                    />
+                  </div>
 
-                {/* Title */}
-                <div className="p-4 bg-zinc-700">
-                  <h3 className="text-xl font-bold mb-2 text-center">
-                    {item.title}
-                  </h3>
-                </div>
+                  {/* Title */}
+                  <div className="p-4 bg-gray-700">
+                    <h3 className="text-xl font-bold mb-2 text-center">
+                      {item.title}
+                    </h3>
+                  </div>
+                </Link>
               </div>
             ))}
           </div>
