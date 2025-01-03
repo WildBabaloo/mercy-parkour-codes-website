a0afef808ea262a4e26fae3a9b450d420a2632ac
WildBabaloo <andphiltre@gmail.com>
2025-01-02 23:15:25 -0500

    Other placeholder pages

---
+++ app/tech/slingshot/page.tsx
@@ -0,0 +1,10 @@
+import NavBarUI from "@/components/ui/NavBar";
+
+export default function Slingshot() {
+  return (
+    <>
+      <NavBarUI />
+      Slingshot Page
+    </>
+  );
+}
---
+++ app/tech/superjump/page.tsx
@@ -0,0 +1,10 @@
+import NavBarUI from "@/components/ui/NavBar";
+
+export default function Superjump() {
+  return (
+    <>
+      <NavBarUI />
+      Superjump Page
+    </>
+  );
+}
