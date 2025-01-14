d9176d46dc1a118d8be4180a78f723640a013109
WildBabaloo <andphiltre@gmail.com>
2025-01-14 18:46:06 -0500

    Still stuck Crouch GA Bhop page

--- app/tech/crouch_ga_bhop/page.tsx
+++ app/tech/crouch_ga_bhop/page.tsx
@@ -1,4 +1,6 @@
+import If_You_Are_Still_Stuck_Text from "@/components/if_you_are_still_stuck_text";
 import NavBarUI from "@/components/ui/NavBar";
+import Link from "next/link";
 
 export default function Crouch_GA_Bhop() {
   return (
@@ -120,6 +122,57 @@ export default function Crouch_GA_Bhop() {
               </p>
             </li>
           </ul>
+        </div>
+
+        {/* Still Stuck Section */}
+        <div className="border border-gray-600 rounded-lg p-6 bg-gray-700 shadow-md my-8 mx-auto max-w-4xl">
+          <h3 className="text-xl font-semibold mb-4 text-yellow-400">
+            Still Stuck?
+          </h3>
+          <div className="text-base leading-relaxed">
+            <p>
+              Consider the solution to be (especially if crouch is disabled):
+            </p>
+            <p>
+              1-{" "}
+              <Link
+                href="/tech/walking_slant_jump"
+                className="text-blue-400 hover:text-blue-500 underline transition-colors duration-300"
+              >
+                Walking Slant Jump
+              </Link>
+            </p>
+            <p>
+              2-{" "}
+              <Link
+                href="/tech/jumping_slant_jump"
+                className="text-blue-400 hover:text-blue-500 underline transition-colors duration-300"
+              >
+                Jumping Slant Jump
+              </Link>
+            </p>
+            <p>
+              3-{" "}
+              <Link
+                href="/tech/reverse_walking_slant_jump"
+                className="text-blue-400 hover:text-blue-500 underline transition-colors duration-300"
+              >
+                Reverse Walking Slant Jump
+              </Link>
+            </p>
+            <p>
+              4-{" "}
+              <Link
+                href="/tech/reverse_jumping_slant_jump"
+                className="text-blue-400 hover:text-blue-500 underline transition-colors duration-300"
+              >
+                Reverse Jumping Slant Jump
+              </Link>
+            </p>
+            <div className="mt-8 font-semibold">
+              <If_You_Are_Still_Stuck_Text />
+            </div>
+          </div>
         </div>
       </div>
     </>
