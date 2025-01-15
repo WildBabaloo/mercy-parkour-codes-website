947cc636f136f6056f0c39afb0334078f4a39381
WildBabaloo <andphiltre@gmail.com>
2025-01-14 19:12:59 -0500

    Still stuck stuck (ha!) page

--- app/tech/stuck/page.tsx
+++ app/tech/stuck/page.tsx
@@ -1,4 +1,6 @@
+import If_You_Are_Still_Stuck_Text from "@/components/if_you_are_still_stuck_text";
 import NavBarUI from "@/components/ui/NavBar";
+import Link from "next/link";
 
 export default function Stuck() {
   return (
@@ -114,6 +116,53 @@ export default function Stuck() {
               upwards can help you reach the checkpoint
             </li>
           </ul>
+        </div>
+
+        {/* Still Stuck Section */}
+        <div className="border border-gray-600 rounded-lg p-6 bg-gray-700 shadow-md my-8 mx-auto max-w-4xl">
+          <h3 className="text-xl font-semibold mb-4 text-yellow-400">
+            Still Stuck?
+          </h3>
+          <div className="text-base leading-relaxed">
+            <p>Consider the solution to be:</p>
+            <p>
+              1-{" "}
+              <Link
+                href="/tech/sit_emote"
+                className="text-blue-400 hover:text-blue-500 underline transition-colors duration-300"
+              >
+                Sit Emote
+              </Link>
+            </p>
+            <p>
+              2-{" "}
+              <Link
+                href="/tech/bunnyhop"
+                className="text-blue-400 hover:text-blue-500 underline transition-colors duration-300"
+              >
+                Bhop
+              </Link>
+            </p>
+            <p>
+              3-{" "}
+              <Link
+                href="/tech/cancel_ga_slingshot"
+                className="text-blue-400 hover:text-blue-500 underline transition-colors duration-300"
+              >
+                Cancel GA Sling
+              </Link>
+              /
+              <Link
+                href="/tech/cancel_ga_superjump"
+                className="text-blue-400 hover:text-blue-500 underline transition-colors duration-300"
+              >
+                Superjump
+              </Link>
+            </p>
+            <div className="mt-8 font-semibold">
+              <If_You_Are_Still_Stuck_Text />
+            </div>
+          </div>
         </div>
       </div>
     </>
