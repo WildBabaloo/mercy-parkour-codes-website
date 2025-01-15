5deffa94d13b8f470c1dd3d73e117972f8c1c02e
WildBabaloo <andphiltre@gmail.com>
2025-01-14 19:18:37 -0500

    Still stuck cancel ga sling page

--- app/tech/cancel_ga_slingshot/page.tsx
+++ app/tech/cancel_ga_slingshot/page.tsx
@@ -1,4 +1,6 @@
+import If_You_Are_Still_Stuck_Text from "@/components/if_you_are_still_stuck_text";
 import NavBarUI from "@/components/ui/NavBar";
+import Link from "next/link";
 
 export default function Cancel_GA_Slingshot() {
   return (
@@ -90,6 +92,37 @@ export default function Cancel_GA_Slingshot() {
               slings will not go as far as a normal fully charged slingshot
             </li>
           </ul>
+        </div>
+
+        {/* Still Stuck Section */}
+        <div className="border border-gray-600 rounded-lg p-6 bg-gray-700 shadow-md my-8 mx-auto max-w-4xl">
+          <h3 className="text-xl font-semibold mb-4 text-yellow-400">
+            Still Stuck?
+          </h3>
+          <div className="text-base leading-relaxed">
+            <p>Consider the solution to be:</p>
+            <p>
+              1-{" "}
+              <Link
+                href="/tech/cancel_ga_superjump"
+                className="text-blue-400 hover:text-blue-500 underline transition-colors duration-300"
+              >
+                Cancel GA Superjump
+              </Link>
+            </p>
+            <p>
+              2-{" "}
+              <Link
+                href="/tech/bhop_reset"
+                className="text-blue-400 hover:text-blue-500 underline transition-colors duration-300"
+              >
+                Infinite Bhop/Bhop Reset
+              </Link>
+            </p>
+            <div className="mt-8 font-semibold">
+              <If_You_Are_Still_Stuck_Text />
+            </div>
+          </div>
         </div>
       </div>
     </>
