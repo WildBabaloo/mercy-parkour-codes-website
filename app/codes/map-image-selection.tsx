63b5dab83b74fd90ed156eeae14abc509c1fcc48
WildBabaloo <andphiltre@gmail.com>
2025-01-17 13:42:50 -0500

    Couple of Map images added

--- app/codes/map-image-selection.tsx
+++ app/codes/map-image-selection.tsx
@@ -1,6 +1,11 @@
 import Ecopoint from "@/public/images/Map_Images/EcoPointAntarctica.png";
-import AntarcticPeninsula from "@/public/images/Map_Images/Antarctic_Peninsula.png";
+import Antarctic_Peninsula from "@/public/images/Map_Images/Antarctic_Peninsula.png";
 import Busan from "@/public/images/Map_Images/Busan.png";
+import Ilios from "@/public/images/Map_Images/Ilios.png";
+import Lijiang_Tower from "@/public/images/Map_Images/Lijiang_Tower.png";
+import Nepal from "@/public/images/Map_Images/Nepal.png";
+import Oasis from "@/public/images/Map_Images/Oasis.png";
+import Samoa from "@/public/images/Map_Images/Samoa.png";
 import { StaticImageData } from "next/image";
 
 type MapImages = {
@@ -15,8 +20,13 @@ const MapImageSelection = (mapName: string): StaticImageData => {
 };
 
 const mapImages: MapImages = {
-  "Antarctic Peninsula": AntarcticPeninsula,
+  "Antarctic Peninsula": Antarctic_Peninsula,
   Busan: Busan,
+  Ilios: Ilios,
+  "Lijiang Tower": Lijiang_Tower,
+  Nepal: Nepal,
+  Oasis: Oasis,
+  Samoa: Samoa,
 };
 
 export default MapImageSelection;
