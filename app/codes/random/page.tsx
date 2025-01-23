519175b4c8c147ac81c7db6cab8e4a817b57bd45
WildBabaloo <andphiltre@gmail.com>
2025-01-22 23:40:17 -0500

    Refactorize

--- app/codes/random/page.tsx
+++ app/codes/random/page.tsx
@@ -1,7 +1,6 @@
+"use client";
 import NavBarUI from "@/components/ui/NavBar";
-import { Button } from "@/components/ui/button";
-import Card from "@/components/CodeCard";
-import Ecopoint from "@/public/images/Map_Images/EcoPointAntarctica.png";
+import GenerateSection from "./generate-section";
 
 export default function Random() {
   return (
@@ -20,30 +19,8 @@ export default function Random() {
             started!
           </p>
         </div>
-
-        {/* Card Section */}
-        <div className="flex justify-center items-center mt-12">
-          <Card
-            key="3157"
-            title="Ecopoint: Antarctica (Winter)"
-            code="QTXG4"
-            checkpoints="45"
-            difficulty="Intermediate"
-            mapper="OnMyKnees"
-            video="https://www.youtube.com/watch?v=tJMdXxLr7BU"
-            likes={0}
-            imageSrc={Ecopoint}
-          />
-        </div>
-
-        {/* Generate Button */}
-        <div className="text-center mt-8">
-          <Button
-            variant="default"
-            className="px-8 py-3 text-lg font-semibold bg-primary text-white rounded-lg shadow-md hover:bg-primary-dark transition-transform transform hover:scale-105"
-          >
-            Generate
-          </Button>
+        <div>
+          <GenerateSection />
         </div>
       </div>
     </>
