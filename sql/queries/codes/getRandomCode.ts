b292fac25119dc9a045ffa30c62a6670b59bda11
WildBabaloo <andphiltre@gmail.com>
2025-01-25 14:13:06 -0500

    Changed function name

--- sql/queries/codes/getRandomCode.ts
+++ sql/queries/codes/getRandomCode.ts
@@ -1,6 +1,6 @@
 import prisma from "@/prisma/lib/db";
 
-export async function getRandomCode() {
+export async function GetRandomCode() {
     const count = await prisma.mercy_parkour_codes.count();
     const randInt = Math.floor(Math.random() * count) + 1;
     const code = await prisma.mercy_parkour_codes.findFirst({
