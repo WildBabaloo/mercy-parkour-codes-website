3737018ab3b835811bddaf38d8b74182b35b01de
WildBabaloo <andphiltre@gmail.com>
2025-02-01 14:37:36 -0500

    Difficulty Key

--- app/extras/page.tsx
+++ app/extras/page.tsx
@@ -1,10 +1,18 @@
 import NavBarUI from "@/components/ui/NavBar";
+import DifficultyKey from "./difficulty-key";
 
 export default function Extras() {
   return (
     <>
       <NavBarUI />
-      Extras Page
+      <div className="bg-gray-800 text-white min-h-screen p-6">
+        <div className="text-center m-6 text-3xl text-white font-bold">
+          <h1>Extras</h1>
+        </div>
+        <div>
+          <DifficultyKey />
+        </div>
+      </div>
     </>
   );
 }
