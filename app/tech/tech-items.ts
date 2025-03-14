6675b0a20832957be3675a5896777906bf797dbd
WildBabaloo <andphiltre@gmail.com>
2025-03-13 20:00:54 -0400

    Tech images added

--- app/tech/tech-items.ts
+++ app/tech/tech-items.ts
@@ -1,152 +1,184 @@
-import Ecopoint from "@/public/images/Map_Images/EcoPointAntarctica.png";
+import Backwards_Slingshot_Image from "@/public/images/Tech_Images/backwards_slingshot.png";
+import Beamless_Propjump_Image from "@/public/images/Tech_Images/beamless_propjump.png";
+import Bhop_Reset_Image from "@/public/images/Tech_Images/bhop_reset.png";
+import Bunnyhop_Image from "@/public/images/Tech_Images/bunnyhop.png";
+import Cancel_GA_Slingshot_Image from "@/public/images/Tech_Images/cancel_ga_slingshot.png";
+import Cancel_GA_Superjump_Image from "@/public/images/Tech_Images/cancel_ga_superjump.png";
+import Cancel_Sling_Rez_Image from "@/public/images/Tech_Images/cancel_sling_rez.png";
+import Clouds_Image from "@/public/images/Tech_Images/clouds.png";
+import Crouch_GA_Bhop_Image from "@/public/images/Tech_Images/crouch_ga_bhop.png";
+import Crouch_GA_Slingshot_Image from "@/public/images/Tech_Images/crouch_ga_slingshot.png";
+import Crouch_GA_Superjump_Image from "@/public/images/Tech_Images/crouch_ga_superjump.png"
+import GA_Cancel_Bhop_Image from "@/public/images/Tech_Images/ga_cancel_bhop.png"
+import Ghost_GA_Image from "@/public/images/Tech_Images/ghost_ga.png"
+import Instant_Superjump_Image from "@/public/images/Tech_Images/instant_superjump.png"
+import Jumping_Slant_Jump_Image from "@/public/images/Tech_Images/jumping_slant_jump.png"
+import Late_Slingshot_Image from "@/public/images/Tech_Images/late_slingshot.png"
+import Late_Superjump_Image from "@/public/images/Tech_Images/late_superjump.png"
+import Propjump_Image from "@/public/images/Tech_Images/propjump.png"
+import Pseudo_Bunnyhop_Image from "@/public/images/Tech_Images/pseudo_bunnyhop.png"
+import Reverse_Crouch_GA_Image from "@/public/images/Tech_Images/reverse_crouch_ga.png"
+import Reverse_Jumping_Slant_Jump_Image from "@/public/images/Tech_Images/reverse_jumping_slant_jump.png"
+import Reverse_Walking_Slant_Jump_Image from "@/public/images/Tech_Images/reverse_walking_slant_jump.png"
+import Rez_Bhop_Reset_Image from "@/public/images/Tech_Images/rez_bhop_reset.png"
+import Roll_Image from "@/public/images/Tech_Images/roll.png"
+import Sit_Image from "@/public/images/Tech_Images/sit.png"
+import Sling_Rez_Image from "@/public/images/Tech_Images/sling_rez.png"
+import Slingshot_Image from "@/public/images/Tech_Images/slingshot.png"
+import Stuck_Image from "@/public/images/Tech_Images/stuck.png"
+import Superjump_Rez_Cancel_Image from "@/public/images/Tech_Images/superjump_rez_cancel.png"
+import Superjump_Rez_Image from "@/public/images/Tech_Images/superjump_rez.png"
+import Superjump_Image from "@/public/images/Tech_Images/superjump.png"
+import Walking_Slant_Jump_Image from "@/public/images/Tech_Images/walking_slant_jump.png"
+import Wall_Bounce_Image from "@/public/images/Tech_Images/wall_bounce.png"
 
 export const techItems = [
-    { id: 1, title: "Slingshot", gif: Ecopoint, link: "/tech/slingshot" },
+    { id: 1, title: "Slingshot", gif: Slingshot_Image, link: "/tech/slingshot" },
     {
       id: 2,
       title: "Backwards Slingshot",
-      gif: Ecopoint,
+      gif: Backwards_Slingshot_Image,
       link: "/tech/backwards_slingshot",
     },
     {
       id: 3,
       title: "Late Slingshot",
-      gif: Ecopoint,
+      gif: Late_Slingshot_Image,
       link: "/tech/late_slingshot",
     },
-    { id: 4, title: "Superjump", gif: Ecopoint, link: "/tech/superjump" },
+    { id: 4, title: "Superjump", gif: Superjump_Image, link: "/tech/superjump" },
     {
       id: 5,
       title: "Late Superjump",
-      gif: Ecopoint,
+      gif: Late_Superjump_Image,
       link: "/tech/late_superjump",
     },
-    { id: 6, title: "Bunnyhop", gif: Ecopoint, link: "/tech/bunnyhop" },
+    { id: 6, title: "Bunnyhop", gif: Bunnyhop_Image, link: "/tech/bunnyhop" },
     {
       id: 7,
       title: "GA Cancel Bhop",
-      gif: Ecopoint,
+      gif: GA_Cancel_Bhop_Image,
       link: "/tech/ga_cancel_bhop",
     },
-    { id: 8, title: "Sit Emote", gif: Ecopoint, link: "/tech/sit_emote" },
+    { id: 8, title: "Sit Emote", gif: Sit_Image, link: "/tech/sit_emote" },
     {
       id: 9,
       title: "Crouch GA Bhop",
-      gif: Ecopoint,
+      gif: Crouch_GA_Bhop_Image,
       link: "/tech/crouch_ga_bhop",
     },
     {
       id: 10,
       title: "Crouch GA Slingshot",
-      gif: Ecopoint,
+      gif: Crouch_GA_Slingshot_Image,
       link: "/tech/crouch_ga_slingshot",
     },
     {
       id: 11,
       title: "Crouch GA Superjump",
-      gif: Ecopoint,
+      gif: Crouch_GA_Superjump_Image,
       link: "/tech/crouch_ga_superjump",
     },
-    { id: 12, title: "Propjump", gif: Ecopoint, link: "/tech/propjump" },
+    { id: 12, title: "Propjump", gif: Propjump_Image, link: "/tech/propjump" },
     {
       id: 13,
       title: "Beamless Propjump",
-      gif: Ecopoint,
+      gif: Beamless_Propjump_Image,
       link: "/tech/beamless_propjump",
     },
     {
       id: 14,
       title: "Instant Superjump",
-      gif: Ecopoint,
+      gif: Instant_Superjump_Image,
       link: "/tech/instant_superjump",
     },
     {
       id: 15,
       title: "Superjump Rez",
-      gif: Ecopoint,
+      gif: Superjump_Rez_Image,
       link: "/tech/superjump_rez",
     },
-    { id: 16, title: "Sling Rez", gif: Ecopoint, link: "/tech/sling_rez" },
+    { id: 16, title: "Sling Rez", gif: Sling_Rez_Image, link: "/tech/sling_rez" },
     {
       id: 17,
       title: "Cloud Jump/Bounce",
-      gif: Ecopoint,
+      gif: Clouds_Image,
       link: "/tech/cloud_jump",
     },
-    { id: 18, title: "Wall Bounce", gif: Ecopoint, link: "/tech/wall_bounce" },
-    { id: 19, title: "Stuck/Balances", gif: Ecopoint, link: "/tech/stuck" },
-    { id: 20, title: "Roll", gif: Ecopoint, link: "/tech/roll" },
+    { id: 18, title: "Wall Bounce", gif: Wall_Bounce_Image, link: "/tech/wall_bounce" },
+    { id: 19, title: "Stuck/Balances", gif: Stuck_Image, link: "/tech/stuck" },
+    { id: 20, title: "Roll", gif: Roll_Image, link: "/tech/roll" },
     {
       id: 21,
       title: "Reverse Crouch GA",
-      gif: Ecopoint,
+      gif: Reverse_Crouch_GA_Image,
       link: "/tech/reverse_crouch_ga",
     },
     {
       id: 22,
       title: "Cancel GA Slingshot",
-      gif: Ecopoint,
+      gif: Cancel_GA_Slingshot_Image,
       link: "/tech/cancel_ga_slingshot",
     },
     {
       id: 23,
       title: "Cancel GA Superjump",
-      gif: Ecopoint,
+      gif: Cancel_GA_Superjump_Image,
       link: "/tech/cancel_ga_superjump",
     },
     {
       id: 24,
       title: "Bhop Reset/Infinite Bhop",
-      gif: Ecopoint,
+      gif: Bhop_Reset_Image,
       link: "/tech/bhop_reset",
     },
     {
       id: 25,
       title: "Superjump Rez Cancel",
-      gif: Ecopoint,
+      gif: Superjump_Rez_Cancel_Image,
       link: "/tech/superjump_rez_cancel",
     },
     {
       id: 26,
       title: "Cancel Sling Rez",
-      gif: Ecopoint,
+      gif: Cancel_Sling_Rez_Image,
       link: "/tech/cancel_sling_rez",
     },
     {
       id: 27,
       title: "Rez Bhop Reset",
-      gif: Ecopoint,
+      gif: Rez_Bhop_Reset_Image,
       link: "/tech/rez_bhop_reset",
     },
     {
       id: 28,
       title: "Walking Slant Jump",
-      gif: Ecopoint,
+      gif: Walking_Slant_Jump_Image,
       link: "/tech/walking_slant_jump",
     },
     {
       id: 29,
       title: "Jumping Slant Jump",
-      gif: Ecopoint,
+      gif: Jumping_Slant_Jump_Image,
       link: "/tech/jumping_slant_jump",
     },
     {
       id: 30,
       title: "Reverse Walking Slant Jump",
-      gif: Ecopoint,
+      gif: Reverse_Walking_Slant_Jump_Image,
       link: "/tech/reverse_walking_slant_jump",
     },
     {
       id: 31,
       title: "Reverse Jumping Slant Jump",
-      gif: Ecopoint,
+      gif: Reverse_Jumping_Slant_Jump_Image,
       link: "/tech/reverse_jumping_slant_jump",
     },
     {
       id: 32,
       title: "Pseudo Bunnyhop",
-      gif: Ecopoint,
+      gif: Pseudo_Bunnyhop_Image,
       link: "/tech/pseudo_bunnyhop",
     },
-    { id: 33, title: "Ghost GA", gif: Ecopoint, link: "/tech/ghost_ga" },
+    { id: 33, title: "Ghost GA", gif: Ghost_GA_Image, link: "/tech/ghost_ga" },
   ];
