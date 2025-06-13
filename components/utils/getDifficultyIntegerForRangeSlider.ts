911f9a5102b97b55bd851ea035573f8efe5836e9
WildBabaloo <andphiltre@gmail.com>
2025-06-13 17:07:09 -0400

    Bug fix on Ticket 102 complete

--- components/utils/getDifficultyIntegerForRangeSlider.ts
+++ components/utils/getDifficultyIntegerForRangeSlider.ts
@@ -2,7 +2,8 @@ export function GetDifficultyIntegerForRangeSlider(difficultyRange: string) {
     const difficultyRangeArray = difficultyRange.split("-").map(Number);
 
     if (!difficultyRangeArray) { return []; }
-    // if (difficultyRangeArray[0] === difficultyRangeArray[1]) { return [difficultyRangeArray[0]]; }
+    if (difficultyRangeArray[0] === 1 && difficultyRangeArray[1] === 17) { return []; }
+    
     const minDifficultyInteger = difficultyRangeArray[0] === 1 ? difficultyRangeArray[0] : difficultyRangeArray[0] - 1;
     const maxDifficultyInterger = difficultyRangeArray[1] === 17 ? difficultyRangeArray[1] : difficultyRangeArray[1] + 1;
     const difficultyIntegersArray: number[] = [];
