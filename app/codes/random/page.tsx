892afc5729a1ea24b0aa683b01c79e81bcc6bc06
WildBabaloo <andphiltre@gmail.com>
2026-08-25 02:26:20 -0400

    Opt out of client component adoption for /random

--- app/codes/random/page.tsx
+++ app/codes/random/page.tsx
@@ -2,10 +2,6 @@ import NavBarUI from "@/components/ui/NavBar";
 import GenerateSection from "./generate-section";
 import { Metadata } from "next";
 
-// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
-// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
-export const instant = false;
-
 export const metadata: Metadata = {
   title: "Mercy Parkour - Random",
   description: "Get a random mercy parkour code to play around with!",
