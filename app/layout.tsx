4d03a37e0f602a29fd386569def4fee33114d5d6
WildBabaloo <andphiltre@gmail.com>
2026-08-25 02:39:07 -0400

    Opt out of cache component adoption

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,6 @@ import "./globals.css";
 import Script from "next/script";
 import { Toaster } from "@/components/ui/toaster";
 
-// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
-// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
-export const instant = false;
-
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
